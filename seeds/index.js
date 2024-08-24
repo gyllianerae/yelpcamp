16dd9103a92f4f5c66870054465caa14ed8c81d9
gyllianerae <100182300+gyllianerae@users.noreply.github.com>
2024-08-24 20:19:00 +0800

    fixed seeds file

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
 const Campground = require('../models/campground');
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
